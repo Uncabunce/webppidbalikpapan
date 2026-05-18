b4f0a92525a4a248edf8280b689cb2f571db4ba4
nigero <joseahmad04@gmail.com>
2026-05-18 10:58:15 +0800

    email requirement to usn

--- app/Filament/Sawit/Pages/Auth/Login.php
+++ app/Filament/Sawit/Pages/Auth/Login.php
@@ -3,8 +3,29 @@
 namespace App\Filament\Sawit\Pages\Auth;
 
 use Filament\Auth\Pages\Login as BaseLogin;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
 
 class Login extends BaseLogin
 {
     protected string $view = 'filament.sawit.pages.auth.login';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('email')
+                    ->label('Username')
+                    ->required()
+                    ->autocomplete()
+                    ->autofocus()
+                    ->extraInputAttributes(['tabindex' => 1]),
+                TextInput::make('password')
+                    ->label('Password')
+                    ->password()
+                    ->revealable()
+                    ->required()
+                    ->extraInputAttributes(['tabindex' => 2]),
+            ]);
+    }
 }
